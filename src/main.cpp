3b912074aed0644296ec4302ebcac6fa19b6b4f0
Raimundo Lorca Correa <raimundo.lorca.c@ug.uchile.cl>
2026-01-09 11:57:43 -0300

    feat: make dns_tools output its help text as default behavior

--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,9 @@
 int main(int argc, char* argv[]){
     CLI::App dns_tools{"Allows signing of a DNS zone using a PKCS#11 device.\n\nFor more information, visit \"https://github.com/niclabs/dns-tools\"."};
 
+    dns_tools.callback([&](){
+        std::cout << dns_tools.help();
+    });
+
     CLI11_PARSE(dns_tools, argc, argv);
 }
